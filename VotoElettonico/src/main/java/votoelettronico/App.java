32ba7036fd5834252ff44409465ead95eb69e1a8
federico-zen <federico.zenatti@studenti.unimi.it>
2022-02-12 16:53:04 +0100

    Update POM with the correct starting class

--- VotoElettonico/src/main/java/votoelettronico/App.java
+++ VotoElettonico/src/main/java/votoelettronico/App.java
@@ -1,9 +1,36 @@
 package votoelettronico;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class App extends Application {
+
+private  static Scene primaryScene;
+	
 	public static void main(String[] args) {
-
+		
+		 launch();
+		
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/fxml/main.fxml"));
+		Parent content = loader.load(); 
+		
+		Scene main = new Scene(content);
+        primaryScene = main;
+        primaryStage.setScene(main);
+        primaryStage.show();
+		
+	}
+	
+	public static Scene getAppScene() {
+        return primaryScene;
+    }
 
 }
---
+++ VotoElettonico/src/main/java/votoelettronico/controller/MainController.java
@@ -0,0 +1,19 @@
+package votoelettronico.controller;
+
+public class MainController extends Controller{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initData(String value) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
+
