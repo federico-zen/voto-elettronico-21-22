3b074e3d5e4bf55da2f63b6bfad0ca935a0e9707
federico-zen <federico.zenatti@studenti.unimi.it>
2022-02-12 17:02:27 +0100

    Controller Update

--- VotoElettonico/src/main/java/votoelettronico/controller/Controller.java
+++ VotoElettonico/src/main/java/votoelettronico/controller/Controller.java
@@ -1,6 +1,26 @@
 package votoelettronico.controller;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import votoelettronico.App;
+
 public abstract  class Controller {
-	public abstract void init();
-	public abstract void initData(String value);
+	public abstract void init(Object parameter);
+	
+	
+	public void changeView(String path,Object parameters) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+        	loader.setLocation(this.getClass().getResource(path));
+			Parent root =  loader.load();
+			Controller c = loader.getController();
+			c.init(parameters);
+			App.getAppScene().setRoot(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
--- VotoElettonico/src/main/java/votoelettronico/controller/MainController.java
+++ VotoElettonico/src/main/java/votoelettronico/controller/MainController.java
@@ -2,14 +2,10 @@ package votoelettronico.controller;
 
 public class MainController extends Controller{
 
-	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 	@Override
-	public void initData(String value) {
+	public void init(Object parameter) {
 		// TODO Auto-generated method stub
 		
 	}
