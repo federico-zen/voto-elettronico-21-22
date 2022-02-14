1f13ac418b457b76441c9286e9385f30e01b1f2f
federico-zen <federico.zenatti@studenti.unimi.it>
2022-02-14 15:42:10 +0100

    remove eta

--- VotoElettonico/src/main/java/votoelettronico/bean/Elettore.java
+++ VotoElettonico/src/main/java/votoelettronico/bean/Elettore.java
@@ -5,7 +5,7 @@ package votoelettronico.bean;
  */
 public class Elettore extends Utente {
 	//ATTRIBUTI
-	private int eta;
+	
 
 	//COSTRUTTORE
 	/**
@@ -21,29 +21,18 @@ public class Elettore extends Utente {
 	 * @param username
 	 * @param eta
 	 */
-	public Elettore(String nome, String cognome, String username, int eta) {
+	public Elettore(String nome, String cognome, String username) {
 		super(nome, cognome, username);
-		this.eta = eta;
 	}
 
 	//METODI
-	/**
-	 * @return the eta
-	 */
-	public int getEta() {
-		return eta;
-	}
+	
 
-	/**
-	 * @param eta the eta to set
-	 */
-	public void setEta(int eta) {
-		this.eta = eta;
-	}
+
 
 	@Override
 	public String toString() {
-		return "Elettore [nome=" + nome + ", cognome=" + cognome + ", username=" + username + ", eta'=" + eta + "]";
+		return "Elettore [nome=" + nome + ", cognome=" + cognome + ", username=" + username + "]";
 	}
 	
 	
