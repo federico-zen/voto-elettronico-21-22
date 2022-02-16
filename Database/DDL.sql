46c18fecc73faf73c75b4b7f4c416118cb5dce7b
federico-zen <federico.zenatti@studenti.unimi.it>
2022-02-16 16:58:09 +0100

    DDL

--- Database/DDL.sql
+++ Database/DDL.sql
@@ -1,3 +1,10 @@
+/*Questo file contiene la Definizione del Database utilizzato per il progetto*/
+CREATE DATABASE votoelettronico;
+
+USE votoelettronico;
+
+/*Definizione delle Tabelle*/
+
 CREATE TABLE `utente` (
   `nome` varchar(255) NOT NULL,
   `cognome` varchar(255) NOT NULL,
