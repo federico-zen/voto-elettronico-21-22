409e2e3967cc2ebbb061309e57423b707f74874c
zDino99z <matteo.airoldi3@studenti.unimi.it>
2022-02-24 16:20:13 +0100

    creazione cotroller per il gestore

---
+++ VotoElettonico/src/main/java/votoelettronico/controller/HomeGestoreController.java
@@ -0,0 +1,77 @@
+package votoelettronico.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HomeGestoreController {
+
+    @FXML
+    private Button addCandidatoBtn;
+
+    @FXML
+    private Button addPartitoBtn;
+
+    @FXML
+    private Button addUserBtn;
+
+    @FXML
+    private Button deleteCandidatoBtn;
+
+    @FXML
+    private Button deletePartitoBtn;
+
+    @FXML
+    private Button deleteUserBtn;
+
+    @FXML
+    private Button endSessionBtn;
+
+    @FXML
+    private Button newSessionBtn;
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    void addCandidatoBtnEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addPartitoBtnEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addUserBtnEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteCandidatoBtnEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletePartitoBtnEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteUserBtnEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void endSessionBtnEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newSessionBtnEvent(ActionEvent event) {
+
+    }
+
+}
