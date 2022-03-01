8cae86cc4aba7606a25d1be2795bb93202cf73ff
zDino99z <matteo.airoldi3@studenti.unimi.it>
2022-03-01 16:52:52 +0100

    added inserisci modifica candidato java and fxml files

---
+++ VotoElettonico/src/main/java/votoelettronico/controller/InserisciModificaCandidatoController.java
@@ -0,0 +1,49 @@
+package votoelettronico.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class InserisciModificaCandidatoController extends Controller {
+
+    @FXML
+    private Label addLabel;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private TextField cognomeTF;
+
+    @FXML
+    private ComboBox<?> listaPartiti;
+
+    @FXML
+    private TextField nomeTF;
+
+    @FXML
+    private Button saveBtn;
+
+    @FXML
+    private Label updateLabel;
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+	@Override
+	public void init(Object parameters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
