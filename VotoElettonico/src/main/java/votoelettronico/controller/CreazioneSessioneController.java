3bda0b9a06fc0ccd57e35d627a3c82d30b1b513a
zDino99z <matteo.airoldi3@studenti.unimi.it>
2022-03-02 17:11:33 +0100

    added creazione sessione java and fxml files

---
+++ VotoElettonico/src/main/java/votoelettronico/controller/CreazioneSessioneController.java
@@ -0,0 +1,73 @@
+package votoelettronico.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class CreazioneSessioneController extends Controller {
+
+    @FXML
+    private Button aggiungiBtn;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private TextField domandaTF;
+
+    @FXML
+    private TextField nomeSessioneTF;
+
+    @FXML
+    private ListView<?> partitiAggiuntiLV;
+
+    @FXML
+    private ListView<?> partitiInseribiliLV;
+
+    @FXML
+    private Button rimuoviBtn;
+
+    @FXML
+    private Button startBtn;
+
+    @FXML
+    private ComboBox<?> tipologiaVittoriaCB;
+
+    @FXML
+    private ComboBox<?> tipologiaVotoCB;
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void remove(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectedVoto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void start(ActionEvent event) {
+
+    }
+
+	@Override
+	public void init(Object parameters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
