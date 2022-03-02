49242f15d46d7dd4aa5eabadf29730d82e303d4a
federico-zen <federico.zenatti@studenti.unimi.it>
2022-03-02 14:43:20 +0100

    sessione DAO

---
+++ VotoElettonico/src/main/java/votoelettronico/dao/SessionDAO.java
@@ -0,0 +1,39 @@
+package votoelettronico.dao;
+
+import java.util.List;
+
+import votoelettronico.model.Sessione;
+
+public class SessionDAO implements GenericDAO<Sessione> {
+
+	@Override
+	public Sessione get(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Sessione> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Sessione t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Sessione t, String[] params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Sessione t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
