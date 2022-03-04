8faf0eb85dae66c021643c8de7bb11df56c7f9d4
zDino99z <matteo.airoldi3@studenti.unimi.it>
2022-03-04 16:04:04 +0100

    added voto categorico con preferenze files

--- VotoElettonico/src/main/java/votoelettronico/controller/CategoricoPreferenzeController.java
+++ VotoElettonico/src/main/java/votoelettronico/controller/CategoricoPreferenzeController.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import votoelettronico.factory.AlertFactory;
 import votoelettronico.model.Elettore;
 import votoelettronico.model.Sessione;
 
-public class ReferendumController extends Controller {
+public class CategoricoPreferenzeController extends Controller {
 	
 	Elettore logged;
 	Sessione s = null;
@@ -25,28 +25,31 @@ public class ReferendumController extends Controller {
     private Button confirmBtn;
 
     @FXML
-    private TextArea domanda;
+    private ListView<?> listaCandidati;
+
+    @FXML
+    private ListView<?> listaPartiti;
+
+    @FXML
+    private ImageView logo;
 
     @FXML
     private Label nomeSessioneLabel;
-
-    @FXML
-    private ToggleGroup scelta;
 
     @FXML
     private Button whiteBtn;
 
     @FXML
     void astenuto(ActionEvent event) {
-    	
+    	//alert conferma
     	Alert alert = AlertFactory.getInstance().getSlimAlert(AlertType.CONFIRMATION, "Sei sicuro di voler confermare l'astensione?");
     	alert.showAndWait();
     	
     	if(alert.getResult() == ButtonType.OK) {
     		AlertFactory.getInstance().getSlimAlert(AlertType.INFORMATION, "Azione Registrata!").showAndWait();
+    		
     		//Carica Voto
     		//Change View
-    		
     	}
     }
 
@@ -61,8 +64,13 @@ public class ReferendumController extends Controller {
     		
     		//Carica Voto
     		//Change View
-    		
     	}
+    		
+    }
+
+    @FXML
+    void showCandidati(MouseEvent event) {
+
     }
 
 	@Override
@@ -70,8 +78,6 @@ public class ReferendumController extends Controller {
 		List l = (List) parameters;
 		logged = (Elettore) l.get(0);
 		s = (Sessione) l.get(1);
-		domanda.setText(s.getDomanda());
-		domanda.setWrapText(true);
 		nomeSessioneLabel.setText(s.getNome());
 	}
 
