c0aa29538feceb3aa7f59f991be4824fa28e4ba0
federico-zen <federico.zenatti@studenti.unimi.it>
2022-03-07 17:14:44 +0100

    domain test

--- VotoElettonico/src/test/java/test/TestDominio.java
+++ VotoElettonico/src/test/java/test/TestDominio.java
@@ -1,17 +1,12 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.SQLException;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import votoelettronico.dbconnection.DBConnection;
 import votoelettronico.model.Candidato;
 import votoelettronico.model.Partito;
 import votoelettronico.model.Sessione;
@@ -23,7 +18,7 @@ public class TestDominio {
 	
 	@Test
 	public  void testPartito() {
-		Candidato r = new Candidato("c3","c4");
+		
 		Partito t = new Partito("Partito",List.of(new Candidato("c1", "c1"),new Candidato("c2", "c2") ),null);
 		assertNotNull(t.iterator().next());
 		assertTrue(t.isPartito());
@@ -33,10 +28,11 @@ public class TestDominio {
 	@Test
 	public  void testSessione() {
 		
-		Sessione s = new Sessione("nome", "Referendum", "Quorum", null, true, null);
+		Sessione s = new Sessione("nome", "Referendum", "Quorum", "Domanda", true, null);
 		
 		if(s.getMod_voto().equalsIgnoreCase("Referendum")) {
-			assertNotNull(s.getDomanda());
+			
+			assertTrue(s.getDomanda().length()>0);
 		}
 		
 		assertTrue(s.getStato());
